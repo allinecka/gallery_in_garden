58c9d82c81686aa869fcc27afe86302c7d44b865
Alena Zachová <azachova@gmail.com>
2020-08-24 10:29:24 +0200

    without bootstrap

--- style.js
+++ style.js
@@ -12,6 +12,7 @@ $('a.elipse').on('click', function(e) {
   e.preventDefault();
 });
 
+//button circle
 function prepnoutTridu(element, trida) {
   if (element.className.match(trida)) {
     element.className = element.className.replace(trida, "");
@@ -20,3 +21,28 @@ function prepnoutTridu(element, trida) {
     element.className += " " + trida;
   }
 }
+//carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  //dots[slideIndex-1].className += " active";
+}
+
